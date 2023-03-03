1d27aab747737149c0b54e108284555bc4ed6d5a
unknown <fauzangustifa@gmail.com>
2023-03-03 08:36:30 +0700

    front

---
+++ resources/views/front/layout/js_header.blade.php
@@ -0,0 +1,18 @@
+<!-- All Javascripts -->
+<script src="{{asset('front/js/jquery-3.6.1.min.js')}}"></script>
+        <script src="{{asset('front/js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('front/js/bootstrap-datepicker.min.js')}}"></script>
+        <!--<script src="js/jquery-ui.js"></script>-->
+        <script src="{{asset('front/js/jquery.magnific-popup.min.js')}}"></script>
+        <script src="{{asset('front/js/owl.carousel.min.js')}}"></script>
+        <script src="{{asset('front/js/wow.min.js')}}"></script>
+        <script src="{{asset('front/js/select2.full.js')}}"></script>
+        <script src="{{asset('front/js/jquery.waypoints.min.js')}}"></script>
+        <script src="{{asset('front/js/moment.min.js')}}"></script>
+        <script src="{{asset('front/js/jquery.meanmenu.js')}}"></script>
+        <script src="{{asset('front/tinymce/tinymce.min.js')}}"></script>
+
+        <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+        />
---
+++ resources/views/front/layout/slider.blade.php
@@ -0,0 +1,105 @@
+<div class="slider" style="background-image: url({{asset('front/uploads/banner5.jpg')}})">
+            <div class="bg"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="item">
+                            <div class="text">
+                                <h2>Find Your Desired Job</h2>
+                                <p>
+                                    Search the best, perfect and suitable jobs
+                                    that matches your skills in your expertise
+                                    area.
+                                </p>
+                            </div>
+                            <div class="search-section">
+                                <form action="jobs.html" method="post">
+                                    <div class="inner">
+                                        <div class="row">
+                                            <div class="col-lg-3">
+                                                <div class="form-group">
+                                                    <input
+                                                        type="text"
+                                                        name=""
+                                                        class="form-control"
+                                                        placeholder="Job Title"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-3">
+                                                <div class="form-group">
+                                                    <select
+                                                        name=""
+                                                        class="form-select select2"
+                                                    >
+                                                        <option value="">
+                                                            Job Location
+                                                        </option>
+                                                        <option value="">
+                                                            Australia
+                                                        </option>
+                                                        <option value="">
+                                                            Bangladesh
+                                                        </option>
+                                                        <option value="">
+                                                            Canada
+                                                        </option>
+                                                        <option value="">
+                                                            China
+                                                        </option>
+                                                        <option value="">
+                                                            India
+                                                        </option>
+                                                        <option value="">
+                                                            United Kingdom
+                                                        </option>
+                                                        <option value="">
+                                                            United States
+                                                        </option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-3">
+                                                <div class="form-group">
+                                                    <select
+                                                        name=""
+                                                        class="form-select select2"
+                                                    >
+                                                        <option value="">
+                                                            Job Category
+                                                        </option>
+                                                        <option value="">
+                                                            Accounting
+                                                        </option>
+                                                        <option value="">
+                                                            Customer Support
+                                                        </option>
+                                                        <option value="">
+                                                            Web Design
+                                                        </option>
+                                                        <option value="">
+                                                            Web Development
+                                                        </option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="col-lg-3">
+                                                <button
+                                                    type="submit"
+                                                    class="btn btn-primary"
+                                                >
+                                                    <i
+                                                        class="fas fa-search"
+                                                    ></i>
+                                                    Search
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
