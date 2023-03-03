779431748485c11dab5f83f30d3dbfb68393d749
unknown <fauzangustifa@gmail.com>
2023-03-03 08:43:06 +0700

    Front

---
+++ resources/views/front/layout/nav_top.blade.php
@@ -0,0 +1,26 @@
+<div class="top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 left-side">
+                        <ul>
+                            <li class="phone-text">111-222-3333</li>
+                            <li class="email-text">contact@arefindev.com</li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 right-side">
+                        <ul class="right">
+                            <li class="menu">
+                                <a href="login.html"
+                                    ><i class="fas fa-sign-in-alt"></i> Login</a
+                                >
+                            </li>
+                            <li class="menu">
+                                <a href="signup.html"
+                                    ><i class="fas fa-user"></i> Sign Up</a
+                                >
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
